da1cd40f1940697b815851b40aec6e377c213861
Elizabeth Lau <155056712+elizabeth-lau@users.noreply.github.com>
2024-01-18 01:13:33 -0700

    created function ComputerChoice, parameters playerSelection and computerSelection

---
+++ main.js
@@ -0,0 +1,43 @@
+const options = ["rock" , "paper", "scissors"]
+
+
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random()* options.length)];
+    console.log(choice);
+    return choice;
+}
+
+function checkWinner (playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie"; 
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player"; 
+    }
+    else{
+        return "Computer";
+    }
+}
+
+function playRound (playerSelection, computerSelection){
+    const result = checkWinner (playerSelection, computerSelection);
+    if(result == "Tie") {
+        return "It's a Tie!"
+    }
+    else if( result == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection)); 
+
+getComputerChoice();
